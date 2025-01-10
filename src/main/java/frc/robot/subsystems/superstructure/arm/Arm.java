c539817f7affb2e6a9259917baa339447d930524
MqxS <maxgoog06@outlook.com>
2025-01-10 13:57:23 -0500

    Added sysid to arm and added to Robot.java

--- src/main/java/frc/robot/subsystems/superstructure/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/superstructure/arm/Arm.java
@@ -1,16 +1,26 @@
 package frc.robot.subsystems.superstructure.arm;
 
-import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.units.CurrentUnit;
+import edu.wpi.first.units.VoltageUnit;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.Constants;
 import frc.robot.constants.HardwareConstants;
 import frc.robot.utils.logging.LogUtils;
 import org.littletonrobotics.junction.Logger;
+
+import static edu.wpi.first.units.Units.*;
 
 public class Arm extends SubsystemBase {
     protected static final String LogKey = "Arm";
@@ -20,8 +30,8 @@ public class Arm extends SubsystemBase {
     private final ArmIO armIO;
     private final ArmIOInputsAutoLogged inputs;
 
-//    private final SysIdRoutine voltageSysIdRoutine;
-//    private final SysIdRoutine torqueCurrentSysIdRoutine;
+    private final SysIdRoutine voltageSysIdRoutine;
+    private final SysIdRoutine torqueCurrentSysIdRoutine;
 
     private Goal desiredGoal = Goal.STOW;
     private Goal currentGoal = desiredGoal;
@@ -71,6 +81,17 @@ public class Arm extends SubsystemBase {
         };
 
         this.inputs = new ArmIOInputsAutoLogged();
+
+        this.voltageSysIdRoutine = makeVoltageSysIdRoutine(
+                Volts.of(4).per(Second),
+                Volts.of(8),
+                Seconds.of(6)
+        );
+        this.torqueCurrentSysIdRoutine = makeTorqueCurrentSysIdRoutine(
+                Amps.of(2).per(Second),
+                Amps.of(8),
+                Seconds.of(6)
+        );
 
         this.setpoint = new PositionSetpoint();
         this.pivotLowerSetpoint = new PositionSetpoint().withPivotPositionRots(constants.lowerLimitRots());
@@ -123,5 +144,69 @@ public class Arm extends SubsystemBase {
         this.desiredGoal = goal;
         Logger.recordOutput(LogKey + "/CurrentGoal", currentGoal.toString());
         Logger.recordOutput(LogKey + "/DesiredGoal", desiredGoal.toString());
+    }
+
+    private SysIdRoutine makeVoltageSysIdRoutine(
+            final Velocity<VoltageUnit> voltageRampRate,
+            final Voltage stepVoltage,
+            final Time timeout
+    ) {
+        return new SysIdRoutine(
+                new SysIdRoutine.Config(
+                        voltageRampRate,
+                        stepVoltage,
+                        timeout,
+                        state -> SignalLogger.writeString(String.format("%s-state", LogKey), state.toString())
+                ),
+                new SysIdRoutine.Mechanism(
+                        voltageMeasure -> armIO.toPivotVoltage(
+                                voltageMeasure.in(Volts)
+                        ),
+                        null,
+                        this
+                )
+        );
+    }
+
+    private SysIdRoutine makeTorqueCurrentSysIdRoutine(
+            final Velocity<CurrentUnit> currentRampRate,
+            final Current stepCurrent,
+            final Time timeout
+    ) {
+        return new SysIdRoutine(
+                new SysIdRoutine.Config(
+                        Volts.per(Second).of(currentRampRate.baseUnitMagnitude()),
+                        Volts.of(stepCurrent.baseUnitMagnitude()),
+                        timeout,
+                        state -> SignalLogger.writeString(String.format("%s-state", LogKey), state.toString())
+                ),
+                new SysIdRoutine.Mechanism(
+                        voltageMeasure -> armIO.toPivotTorqueCurrent(
+                                voltageMeasure.in(Volts)
+                        ),
+                        null,
+                        this
+                )
+        );
+    }
+
+    private Command makeSysIdCommand(final SysIdRoutine sysIdRoutine) {
+        return Commands.sequence(
+                sysIdRoutine.quasistatic(SysIdRoutine.Direction.kForward).until(atPivotUpperLimit),
+                Commands.waitSeconds(4),
+                sysIdRoutine.quasistatic(SysIdRoutine.Direction.kReverse).until(atPivotLowerLimit),
+                Commands.waitSeconds(4),
+                sysIdRoutine.dynamic(SysIdRoutine.Direction.kForward).until(atPivotUpperLimit),
+                Commands.waitSeconds(4),
+                sysIdRoutine.dynamic(SysIdRoutine.Direction.kReverse).until(atPivotLowerLimit)
+        );
+    }
+
+    public Command voltageSysIdCommand() {
+        return makeSysIdCommand(voltageSysIdRoutine);
+    }
+
+    public Command torqueCurrentSysIdCommand() {
+        return makeSysIdCommand(torqueCurrentSysIdRoutine);
     }
 }
