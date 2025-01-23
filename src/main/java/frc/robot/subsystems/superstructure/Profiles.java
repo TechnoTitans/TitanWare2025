defb672113f6e73ab5c90a4a067fdbae0941811f
MqxS <maxgoog06@outlook.com>
2025-01-23 11:03:19 -0500

    initial profiling

--- src/main/java/frc/robot/subsystems/superstructure/Profiles.java
+++ src/main/java/frc/robot/subsystems/superstructure/Profiles.java
@@ -2,111 +2,111 @@ package frc.robot.subsystems.superstructure;
 
 public class Profiles {
     public static final SplineProfile L1_TO_L2 = new SplineProfile(
-            5,
+            0.5,
             Superstructure.Goal.L1,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.16,
+            0.028,
+            0.387,
+            -0.047,
             Superstructure.Goal.L2
     );
     public static final SplineProfile L1_TO_L3 = new SplineProfile(
-            5,
+            0.7,
             Superstructure.Goal.L1,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.16,
+            0.028,
+            0.591,
+            0,
             Superstructure.Goal.L3
     );
     public static final SplineProfile L1_TO_L4 = new SplineProfile(
-            5,
+            1.1,
             Superstructure.Goal.L1,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.579,
+            0.745,
+            0.922,
+            0.261,
             Superstructure.Goal.L4
     );
     public static final SplineProfile L2_TO_L1 = new SplineProfile(
-            5,
+            0.57,
             Superstructure.Goal.L2,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.445,
+            -0.047,
+            0.16,
+            0.028,
             Superstructure.Goal.L1
     );
     public static final SplineProfile L2_TO_L3 = new SplineProfile(
-            5,
+            0.74,
             Superstructure.Goal.L2,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.724,
+            0.139,
+            0.748,
+            0.482,
             Superstructure.Goal.L3
     );
     public static final SplineProfile L2_TO_L4 = new SplineProfile(
-            0.89,
+            0.99,
             Superstructure.Goal.L2,
-            0.876,
-            0.226,
-            1.015,
-            0.854,
+            0.724,
+            0.139,
+            0.858,
+            0.400,
             Superstructure.Goal.L4
     );
     public static final SplineProfile L3_TO_L1 = new SplineProfile(
             5,
             Superstructure.Goal.L3,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.591,
+            0,
+            0.16,
+            0.028,
             Superstructure.Goal.L1
     );
     public static final SplineProfile L3_TO_L2 = new SplineProfile(
             5,
             Superstructure.Goal.L3,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.748,
+            0.482,
+            0.724,
+            0.139,
             Superstructure.Goal.L2
     );
     public static final SplineProfile L3_TO_L4 = new SplineProfile(
             5,
             Superstructure.Goal.L3,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.829,
+            0.540,
+            0.934,
+            0.732,
             Superstructure.Goal.L4
     );
     public static final SplineProfile L4_TO_L1 = new SplineProfile(
             5,
             Superstructure.Goal.L4,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.922,
+            0.261,
+            0.579,
+            0.075,
             Superstructure.Goal.L1
     );
     public static final SplineProfile L4_TO_L2 = new SplineProfile(
             5,
             Superstructure.Goal.L4,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.858,
+            0.400,
+            0.724,
+            0.139,
             Superstructure.Goal.L2
     );
     public static final SplineProfile L4_TO_L3 = new SplineProfile(
             5,
             Superstructure.Goal.L4,
-            1.047,
-            1,
-            1.117,
-            1,
+            0.934,
+            0.732,
+            0.829,
+            0.540,
             Superstructure.Goal.L3
     );
 }
