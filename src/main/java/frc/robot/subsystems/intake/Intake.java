ed10847e6d56ee2c17cbc596751c86533f68237e
MqxS <maxgoog06@outlook.com>
2025-03-14 19:28:59 -0400

    Update at 'Fri Mar 14 19:28:59 EDT 2025'

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -197,7 +197,7 @@ public class Intake extends SubsystemBase {
     }
 
     public Command holdCoral() {
-        return toInstantRollerTorqueCurrent(20).withName("HoldCoral");
+        return toInstantRollerTorqueCurrent(15).withName("HoldCoral");
     }
 
     public Command holdAlgae() {
