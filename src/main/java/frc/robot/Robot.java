304f0b84097ada23cfe1adc2dfa86b5c6098da8c
harry.chen <harryxtchen@gmail.com>
2025-03-30 13:00:30 -0400

    Make sim automatically pick Blue alliance and connect DS

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,12 @@
 package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
+import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -15,6 +17,7 @@ import frc.robot.auto.AutoChooser;
 import frc.robot.auto.AutoOption;
 import frc.robot.auto.Autos;
 import frc.robot.constants.Constants;
+import frc.robot.constants.FieldConstants;
 import frc.robot.constants.HardwareConstants;
 import frc.robot.constants.RobotMap;
 import frc.robot.selector.BranchSelector;
@@ -215,6 +218,9 @@ public class Robot extends LoggedRobot {
 //                SignalLogger.setPath("/logs");
                 Logger.addDataReceiver(new WPILOGWriter(""));
                 Logger.addDataReceiver(new NT4Publisher());
+
+                DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
+                DriverStationSim.notifyNewData();
             }
             case REPLAY -> {
                 setUseTiming(false);
