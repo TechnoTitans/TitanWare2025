6d63f3d3ff1de61e91090c11084ae8a4df024bbb
MqxS <maxgoog06@outlook.com>
2025-04-03 16:23:56 -0400

    Update at 'Thu Apr 03 16:23:55 EDT 2025'

--- src/main/java/frc/robot/subsystems/superstructure/distal/IntakeArm.java
+++ src/main/java/frc/robot/subsystems/superstructure/distal/IntakeArm.java
@@ -159,6 +159,7 @@ public class IntakeArm extends SubsystemBase {
                 algaeSlowGoal.position = positionSetpoint.pivotPositionRots;
                 algaeSlowGoal.velocity = 0;
                 mode = Mode.ALGAE_SLOW;
+                if (true) throw new RuntimeException();
             } else {
                 intakeArmIO.toPivotPosition(positionSetpoint.pivotPositionRots);
                 mode = Mode.NORMAL;
