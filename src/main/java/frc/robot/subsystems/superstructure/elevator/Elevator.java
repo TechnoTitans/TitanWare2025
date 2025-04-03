aceb898f80f6bca954ce33f316eb165f39f4e4db
MqxS <maxgoog06@outlook.com>
2025-04-03 17:52:00 -0400

    Update at 'Thu Apr 03 17:52:00 EDT 2025'

--- src/main/java/frc/robot/subsystems/superstructure/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/superstructure/elevator/Elevator.java
@@ -79,7 +79,7 @@ public class Elevator extends SubsystemBase {
     public enum Goal {
         DYNAMIC(0),
         STOW(0.01),
-        CLIMB_DOWN(0.0255),
+        CLIMB_DOWN(0.0765),
 //        CLIMB_DOWN(0.051),
         HP(0.065),
         ALGAE_GROUND(0),
